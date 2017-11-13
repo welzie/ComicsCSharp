cfbe582dba4a707cf69c9445115126ed0020b8f4
Blake <welzie@gmail.com>
2017-11-13 15:53:46 -0800

    Add project files.

---
+++ ComicsCSharp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ComicsCSharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
