cdc3bddd67f97812c08ee7ad824704a6e7a11fdb
Blake <welzie@gmail.com>
2017-11-30 15:07:08 -0800

    getting ready for unit 3 class 8

---
+++ ComicsCSharp/Controllers/ComicsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ComicsCSharp.Models;
+
+namespace ComicsCSharp.Controllers
+{
+    public class ComicsController : Controller
+    {
+
+        private static List<Comic> comics = new List<Comic>();
+
+        public IActionResult Index()
+        {
+            ViewBag.comics = comics;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(string title, int issueNumber)
+        {
+            var comic = new Comic(title, issueNumber);
+            comics.Add(comic);
+            return Redirect("/Comics");
+        }
+    }
+}
---
+++ ComicsCSharp/Models/Comic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ComicsCSharp.Models
+{
+    public class Comic
+    {
+  
+        public string Title { get; set; }
+        public int IssueNumber { get; set; }
+
+        
+
+        public Comic(string title, int IssueNumber)
+        {
+            this.Title = title;
+            this.IssueNumber = IssueNumber;
+        }
+
+    }
+}
