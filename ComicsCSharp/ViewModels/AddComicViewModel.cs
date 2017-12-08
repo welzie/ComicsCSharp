2820e90f2151dc1fd96a9ebaa3e288f67e6e250b
Blake <welzie@gmail.com>
2017-12-07 17:24:36 -0800

    using list of Grade objects to create select box

--- ComicsCSharp/ViewModels/AddComicViewModel.cs
+++ ComicsCSharp/ViewModels/AddComicViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using ComicsCSharp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -11,13 +12,18 @@ namespace ComicsCSharp.ViewModels
         [Required]
         public int? IssueNumber { get; set; }
         public int? GradeId { get; set; }
-        
-        public List<SelectListItem> SelectItems { get; } = new List<SelectListItem>()
+
+        public List<SelectListItem> SelectItems { get; } = new List<SelectListItem>();
+
+        public AddComicViewModel()
         {
-            new SelectListItem() { Value = "1", Text="Mint"},
-            new SelectListItem() { Value="2",Text="Fair"},
-            new SelectListItem() {Value="3",Text="Poor"}
-        };
+            // Use the Grade objects to create the SelectListItem objects
+            foreach (Grade grade in ComicsData.GetAllGrades())
+            {
+                string idString = grade.GradeId.ToString();
+                this.SelectItems.Add(new SelectListItem() { Value = idString, Text = grade.Name });
+            }
+        }
 
     }
 }
